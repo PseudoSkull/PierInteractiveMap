2991c6302e727227bbf45b0b1c80c67181e8a0a9
PseudoSkull <pseudoskull1@gmail.com>
2024-09-13 17:52:50 -0500

    Basic React app created, with our marina map on the front page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from '/map_for_inkscape.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <img src={reactLogo} alt="Map of the marina" />
+    </>
+  )
+}
+
+export default App
