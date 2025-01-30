ce29322a4918c417d28c47f59a83986575f45c0c
PseudoSkull <pseudoskull1@gmail.com>
2025-01-29 20:33:02 -0600

    Update script for db push

--- backend/scripts/upload_boats.py
+++ backend/scripts/upload_boats.py
@@ -3,7 +3,7 @@ import requests
 
 # Configuration
 CSV_FILE = 'sample_data/boats.csv'  # Replace with the path to your CSV file
-API_URL = 'http://localhost:5000/boats'  # Base URL for your API
+API_URL = 'http://localhost:5000/boat_listings'  # Base URL for your API
 
 # Load CSV Data
 def load_csv(file_path):
@@ -17,9 +17,11 @@ def create_payload(row):
         "customer_name": row["Customer Name"] if pd.notnull(row["Customer Name"]) else None,
         "size": row["Size"] if pd.notnull(row["Size"]) else None,
         "make_model": row["Type of Vehicle"] if pd.notnull(row["Type of Vehicle"]) else None,
+        # "mapped": False,
         "vehicle_type": row["Type"] if pd.notnull(row["Type"]) else None,
         "notes": row["Notes"] if pd.notnull(row["Notes"]) else None,
         "section": "",  # Add a default value for section if not present in your CSV
+        "boat_on_map_id": None,
     }
 
 # Push Data to API
@@ -27,7 +29,8 @@ def push_data_to_api(row):
     payload = create_payload(row)
     response = requests.post(API_URL, json=payload)
     if response.status_code == 201:
-        print(f"Successfully added: {payload['name']} with ID: {response.json()['id']}")
+        print(response.json())
+        print(f"Successfully added: {payload['name']} with ID: {response.json()['boat_listing_id']}")
     else:
         print(f"Failed to add: {payload['name']}. Error: {response.text}")
 
